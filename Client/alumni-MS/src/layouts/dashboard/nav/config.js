43ef91fe3739ec23acc521d3c8d816b70e90ee99
shuja-shah <shujashah786@gmail.com>
2023-04-02 13:55:02 +0500

    Add Client

---
+++ Client/alumni-MS/src/layouts/dashboard/nav/config.js
@@ -0,0 +1,26 @@
+// component
+import SvgColor from '../../../components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
+
+const navConfig = [
+  {
+    title: 'dashboard',
+    path: '/dashboard/app',
+    icon: icon('ic_analytics'),
+  },
+  {
+    title: 'users',
+    path: '/dashboard/user',
+    icon: icon('ic_user'),
+  },
+  {
+    title: 'Jobs',
+    path: '/dashboard/products',
+    icon: icon('ic_cart'),
+  },
+];
+
+export default navConfig;
