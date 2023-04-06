3034e7914c9ed4e77130a9bf12b4cee4c6465991
shuja-shah <shujashah786@gmail.com>
2023-04-06 20:46:48 +0500

    Finalize Backed

--- be/routes/NotificationRoutes.js
+++ be/routes/NotificationRoutes.js
@@ -6,9 +6,13 @@ const { authCheck } = require("../middlewares/_auth");
 router.get('/', authCheck, async (req, res) => {
     try {
         const notifications = await Notification.find();
+        if (!notifications || !notifications.length) {
+            return res.status(404).send({ error: 'There are no recent Notifications' });
+        }
         res.send(notifications);
     } catch (error) {
         res.status(500).send({ error: error.message });
     }
 });
 
+module.exports = router;
--- be/server.js
+++ be/server.js
@@ -9,6 +9,7 @@ const authRoutes = require('./routes/authRoutes');
 const detRoutes = require('./routes/users');
 const jobRoutes = require('./routes/JobRoutes');
 const chat = require('./routes/ChatRoutes');
+const notification = require('./routes/NotificationRoutes');
 
 app.use(cors());
 
@@ -22,6 +23,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', detRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/chat', chat);
+app.use('/api/notification', notification);
 mongoose.connect(mongodb.uri).then(() => {
     app.listen('5000', () => {
         console.log('Listening on 5000');
